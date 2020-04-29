d09a07247fb9a18bfa1ac1bdec2a903311a9f9d2
Michał Kukla <degregar@gmail.com>
2020-04-29 11:36:53 +0200

    Scaffolded plugin

---
+++ dlawp-playlist-replace.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Dlawp Playlist Replace
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     dlawp-playlist-replace
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Dlawp_Playlist_Replace
+ */
+
+// Your code starts here.
